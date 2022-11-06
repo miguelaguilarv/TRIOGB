e7ca85292ae9d05aeb9ac59f1da8bd1c66f7e725
Miguel <miguelaguilar@outlook.cl>
2022-11-06 18:00:57 -0300

    Fragmentos

---
+++ app/src/main/java/com/vocesdelolimpo/triogb/Juegos/OrdenFragment2Nivel.java
@@ -0,0 +1,25 @@
+package com.vocesdelolimpo.triogb.Juegos;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.vocesdelolimpo.triogb.R;
+
+
+public class OrdenFragment2Nivel extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_orden_fragment2_nivel, container, false);
+    }
+
+
+}
